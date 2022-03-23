9e7dc975411a12f0540c4b6618b5485d8aa962df
fred <55216128+f27dxx@users.noreply.github.com>
2022-03-24 06:53:45 +1100

    finish login page and navbar

---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <>
+      <p> Home page Context </p>
+    </>
+  )
+}
+
+export default Home
---
+++ src/pages/TestPage.tsx
@@ -0,0 +1,5 @@
+const TestPage = () => {
+  return <div> I am test page</div>
+}
+
+export default TestPage
